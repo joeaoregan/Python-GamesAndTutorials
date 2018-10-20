76116bd9f93d336f861c6764703db0d7adbffcce
Joe O'Regan <a00258304@student.ait.ie>
2018-10-20 01:42:47 +0100

    Snake Tutorial

---
+++ TechWithTim/TicTacToe-AI/SnakeTutorial.py
@@ -0,0 +1,54 @@
+# Snake Tutorial Python
+
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+class cube(object):
+    rows = 0
+    w = o
+
+    def __init__(self, start, dirnx=1, dirny=0, colour=(255, 0, 0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class snake(object):
+    def __init__(self, colour, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def drawGrid(w, rows, surface):
+    pass
+
+
+def redrawWindos(surface):
+    pass
+
+
+def randomSnack(rows, items):
+    pass
+
+
+def message_box(subject, content):
+    pass
