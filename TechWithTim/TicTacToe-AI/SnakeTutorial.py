cfa85bbb5774dd999dff266ceb68bf2e1a636752
Joe O'Regan <a00258304@student.ait.ie>
2018-10-20 01:54:19 +0100

    Snake Part 1

--- TechWithTim/TicTacToe-AI/SnakeTutorial.py
+++ TechWithTim/TicTacToe-AI/SnakeTutorial.py
@@ -1,4 +1,5 @@
 # Snake Tutorial Python
+# https://www.youtube.com/watch?v=5tvER0MT14s&list=PLzMcBGfZo4-kQNQxlrgl5FtncvEbD0urc
 
 import math
 import random
@@ -9,7 +10,7 @@ from tkinter import messagebox
 
 class cube(object):
     rows = 0
-    w = o
+    w = 0
 
     def __init__(self, start, dirnx=1, dirny=0, colour=(255, 0, 0)):
         pass
@@ -22,8 +23,14 @@ class cube(object):
 
 
 class snake(object):
+    body = []
+    turns = {}
     def __init__(self, colour, pos):
-        pass
+        self.colour = colour
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
 
     def move(self):
         pass
@@ -39,11 +46,21 @@ class snake(object):
 
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBetween = w // rows
+    x,y = 0,0
+    for l in range(rows):
+        x += sizeBetween
+        y += sizeBetween
+
+        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
 
 
-def redrawWindos(surface):
-    pass
+def redrawWindow(win):
+    global rows,width
+    win.fill((0,0,0))
+    drawGrid(width, rows, win)
+    pygame.display.update()
 
 
 def randomSnack(rows, items):
@@ -52,3 +69,22 @@ def randomSnack(rows, items):
 
 def message_box(subject, content):
     pass
+
+def main():
+    global width,rows
+    width, height, rows = 500,500,20
+    win = pygame.display.set_mode((width, height))
+    s = snake((255,0,0), (10,10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+
+        redrawWindow(win)
+
+    pass
+
+main()
